d3c007a2d8b9877457d0e20ae0a756a3e29b9e46
Bobby Boyd <bboyd@sbmcorp.com>
2023-07-31 14:06:45 -0700

    updates

--- lua/boydvoid/config.lua
+++ lua/boydvoid/config.lua
@@ -17,7 +17,6 @@ cmp.setup({
     mapping = {
         -- `Enter` key to confirm completion
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
     }
